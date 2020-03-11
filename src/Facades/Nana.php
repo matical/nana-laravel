9ba8403debdca5e1e829aa15c13eced5f0f209fa
matical <maticalssh@gmail.com>
2020-03-11 12:35:37 +0800

    Add annotation

--- src/Facades/Nana.php
+++ src/Facades/Nana.php
@@ -4,6 +4,7 @@ namespace ksmz\NanaLaravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/** @mixin \ksmz\NanaLaravel\LaravelFetch */
 class Nana extends Facade
 {
     protected static function getFacadeAccessor()
